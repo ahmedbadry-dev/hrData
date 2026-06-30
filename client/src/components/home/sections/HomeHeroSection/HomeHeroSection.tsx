8494a311f617b69f8f5c8ad1b57d688e34f9af53
ahmedbadry-dev <ahmedbadry.dev@gmail.com>
2026-06-30 23:04:26 +0300

    style(HomeHeroSection): comment out hero stamp section for future use

--- client/src/components/home/sections/HomeHeroSection/HomeHeroSection.tsx
+++ client/src/components/home/sections/HomeHeroSection/HomeHeroSection.tsx
@@ -55,13 +55,13 @@ export default function HomeHeroSection() {
       </div>
 
       <div className={styles['hero-right']}>
-        <div className={styles['hero-stamp']}>
+        {/* <div className={styles['hero-stamp']}>
           توظيف
           <br />
           مباشر
           <br />
           ٢٠٢٦
-        </div>
+        </div> */}
         <div>
           <div className={styles['hero-card-preview']}>
             <div className={styles['hcp-tag']}>أرامكو السعودية</div>
